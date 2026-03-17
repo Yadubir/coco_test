0b9295c888ac4920688583d54eb567991d9dc104
Yadubir <yadubir12@gmail.com>
2026-03-17 19:24:21 +0530

    Commited just now

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ print(2*2)
 name = str(input())
 
 print(f"Hi from {name} heheheh !!")
+
+def fibonacci(n):
+  if n <= 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(2))
